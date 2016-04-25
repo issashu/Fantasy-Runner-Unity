5006a66067a88f36372c6be26e55f292db6a74bd
BracedJocker <teodorkirilov2@yahoo.com>
2016-04-25 17:49:26 +0300

    I am truly sorry, my VS decided that the new script is unnecessary. So here is the script and again I would like to apologize for my unprofessionalism.

---
+++ Assets/FollowThePlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowThePlayer : MonoBehaviour {
+
+    public float DistanceFromPlayer;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       Vector2 position = transform.position;
+        position.x = player.transform.position.x +DistanceFromPlayer;
+        transform.position = position;
+	}
+}
